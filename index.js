c28b3dd23bc633c620755410b6d2d1a09c2db7a9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-19 15:51:13 -0500

    rendering view with jade

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+var join = require('path').join;
+
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(morgan('dev'));
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  console.log('rendering index');
+  res.render('index', { title : 'Home' });
+});
+
+var port = 3000;
+app.listen(port);
+console.log('listening at port %d', port);
