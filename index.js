368f2b844904ddab5ff4ac00386f35f4ed12af05
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-19 17:05:06 -0500

    rendering js file from views

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.set('view engine', 'jade');
 app.locals.pretty = true;
 
 app.use(morgan('dev'));
+
+var read = require('fs').readFileSync;
+app.engine('js', function (filePath, options, callback) {
+  console.log('need to render js file %s', filePath);
+  console.log('with options', options);
+  var src = read(filePath);
+  return callback(null, src);
+});
+
+app.get('/js/analytics-config.js', function (req, res) {
+  console.log('rendering js file', req.url);
+  res.render('js/analytics-config.js', {
+    analyticsId: '4xx-xxxxx'
+  });
+});
+
 app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
---
+++ views/js/analytics-config.js
@@ -0,0 +1,1 @@
+var analyticsId = 'default-id';
