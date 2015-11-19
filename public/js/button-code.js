d873f1c7fa8714f5825c5dbcbf7690cd19926d6b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-19 16:33:38 -0500

    Running and eval code from controlled domain is fine

--- public/js/button-code.js
+++ public/js/button-code.js
@@ -1,6 +1,13 @@
-(function initAnalytics(id) {
-  if (!id) {
-    throw new Error('missing analytics id ' + id);
-  }
-  console.log('initialized analytics with id', id);
-}(analyticsId));
+console.log('starting button-code.js');
+
+console.log('can button.js use eval?',
+  eval('"yes"'));
+
+document.getElementById('run').addEventListener('click', function () {
+  var input = document.getElementById('source');
+  var source = input.value;
+  console.log('running source "%s"', source);
+  eval(source);
+});
+
+console.log('finished button-code.js');
